00fe5a67e972dc20b6070b0335a82040622a6a12
Pooya Hatami <am.hatami@gmail.com>
2020-02-12 17:40:08 -0800

    Initial Commit

---
+++ iOS-Swift-SeguesAndNavigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Swift-SeguesAndNavigation
+//
+//  Created by Pooya on 2020-02-12.
+//  Copyright © 2020 centurytrail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
